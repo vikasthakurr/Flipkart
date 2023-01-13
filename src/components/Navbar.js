6cde38aab4d10d909fa9c28fa2b03cc7fa7de6f1
vikasthakurr <vikaskumar20012001@gmail.com>
2023-01-13 16:56:45 +0530

    first changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+   return (
+      <div className="header">
+         <div className="header_first">
+            <img src="https://static-assets- 
+             web.flixcart.com/fk-p-linchpin-web/fk-cp- 
+              zion/img/flipkart-plus_8d85f4.png" 
+               alt="flikart" />
+            <div className="header_first1">
+               <span>Explore</span><span>Plus</span>
+            </div>
+         </div>
+         <div className="header_second"></div>
+         <div className="header_third"></div>
+         <div className="header_fourth"></div>
+         <div className="header_fifth"></div>
+      </div>
+   )
+}
+
+export default Navbar
